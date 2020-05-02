83845bbdefd7aef19bebfc975f89cc2d28d55774
akshayr-tavisca <arajput@tavisca.com>
2020-05-02 17:48:55 +0530

    moved shopping list logic to its own service

--- src/app/shopping-list/service/shopping-list.service.ts
+++ src/app/shopping-list/service/shopping-list.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Ingredient } from 'src/app/shared/models/ingredient.model';
+import { ToastService } from 'src/app/toast/toast-service';
+import { Subject, BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShoppingListService {
+  private ingredients: Array<Ingredient> = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 3),
+  ];
 
-  constructor() { }
+  private ingredients$ = new BehaviorSubject<Array<Ingredient>>(this.ingredients);
+
+  constructor(private toastService: ToastService) {}
+
+  getIngredient() {
+    return this.ingredients$;
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients = [...this.ingredients, ingredient];
+    this.ingredients$.next(this.ingredients);
+    this.toastService.success('Ingredient added successfully!');
+  }
 }
