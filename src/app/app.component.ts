dc802c59d9ffb760bed587161739379061ca20c9
akshayr20 <akshay.rajput5394@gmail.com>
2020-05-02 18:49:53 +0530

    feat: added  routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ToastService } from './toast/toast-service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  loadedFeature = 'recipes';
-
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
-  }
-}
+export class AppComponent {}
