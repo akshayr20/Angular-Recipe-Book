bf1cdf5a27cd2c530059e91a6c91a0e83e1cee73
akshayr20 <akshay.rajput5394@gmail.com>
2020-05-03 20:38:31 +0530

    created interceptor

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,28 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 import { BackendService } from '../shared/services/backend.service';
+import { AuthService } from '../auth/service/auth.service';
+import { Observable } from 'rxjs';
+import { User } from '../auth/model/auth.model';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   collapsed = true;
   navCollapsed = true;
-  constructor(private backendService: BackendService) {}
+  isAuthenticated = false;
+  constructor(
+    private backendService: BackendService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.authService.user.subscribe((user) => {
+      this.isAuthenticated = !!user;
+    });
+  }
 
   saveData() {
     this.backendService.storeRecipes();
